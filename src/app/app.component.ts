d45d904c288929046f29b5a4dc02b0d8830a3dd8
Joy Peris <joyperis11@gmail.com>
2022-03-21 16:53:42 +0300

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { data } from './table';
+
+const jsondata = data;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  
+  columnHeaders: Array<any> = Object.keys(jsondata);
+  height = 0
+  arr: Array<any> = [];
+  cdata: Array<any> = [];
+  properties: Array<any> = [];
+
   title = 'my-app';
+
+  buildTable(property: string) {
+    const cdataarray: Array<any> = [];
+    const d = (jsondata as any)[property];
+ 
+    Object.keys(d).forEach((key: string) => {
+      let e: any = (d as any)[key];
+      e.rowHeader = key;
+      cdataarray.push(e);
+    });
+    return cdataarray;
+  }
+
+  displayedColumns: string[] = ['PROPERTIES', 'Description', 'A', 'B', 'C'];
+
+  calculateHeight(index: number): number {
+    let CardHeight = this.arr[index];
+    return CardHeight;
+  }
+  ngOnInit(): void {
+       
+    this.columnHeaders.forEach((element: string) => {
+      this.height = Object.keys((jsondata as any)[element]).length * 40;
+      this.arr.push(this.height);
+
+    });
+
+    this.columnHeaders.forEach((element: string) => {
+      console.log(this.arr)
+      this.cdata = this.buildTable(element);
+      this.properties.push(this.cdata);
+    });
+   
+  }
 }
---
+++ src/app/table.ts
@@ -0,0 +1,75 @@
+export const data = {
+  Production: {
+    PRODUCTION_HRS: {
+      item1: 10,
+      item2: 20,
+      item3: 30,
+    },
+
+    CYCLE_IN_SEC: {
+      item1: 10,
+      item2: 20,
+      item3: 30,
+    },
+
+    CAVITY: {
+      item1: 10,
+      item2: 20,
+      item3: 30,
+    },
+    PRODUCTION_HRS1: {
+      item1: 10,
+      item2: 20,
+      item3: 30,
+    },
+
+    CYCLE_IN_SEC1: {
+      item1: 10,
+      item2: 20,
+      item3: 30,
+    },
+
+    CAVITY1: {
+      item1: 10,
+      item2: 20,
+      item3: 30,
+    },
+  },
+  Utilities: {
+    POWER_KWH_PER_KG: {
+      item1: 11,
+      item2: 21,
+      item3: 31,
+    },
+
+    UTILITY_KW_PER_KG: {
+      item1: 11,
+      item2: 21,
+      item3: 31,
+    },
+    
+  },
+  Labour: {
+    DIRECT_LABOR: {
+      item1: 1,
+      item2: 2,
+      item3: 3,
+    },
+
+    SUPPORT_LABOR: {
+      item1: 1,
+      item2: 2,
+      item3: 3,
+    },
+    LEAK_TEST: {
+      item1: 1,
+      item2: 2,
+      item3: 3,
+    },
+    TOTAL: {
+      item1: 1,
+      item2: 2,
+      item3: 3,
+    },
+  },
+};
